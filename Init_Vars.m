29d98f2f1d0cf1a0d3492f0b1675181a8847939c
Nishanth Rao <nishantharao@gmail.com>
2019-02-28 22:03:37 +0530

    Add files via upload

---
+++ Init_Vars.m
@@ -0,0 +1,13 @@
+clc;clear;
+
+global Inv;
+
+Inv.M = 0.05;
+Inv.m = 0.2;
+Inv.b = 1;
+Inv.g = 9.81;
+Inv.l = 0.15;
+
+Inv.Kp = 1;
+Inv.Ki = 0;
+Inv.Kd = 0;
---
+++ Initial_Vars.m
@@ -0,0 +1,12 @@
+clc;clear;
+
+global SB;
+
+SB.M = 0.5;
+SB.b = 0.0;
+SB.g = 9.81;
+SB.l = 0.15;
+
+SB.Kp = 1;
+SB.Ki = 0;
+SB.Kd = 0;
